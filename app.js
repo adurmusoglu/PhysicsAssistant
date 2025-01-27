0f9859b372384117ffcca8308d2b7d611448fa8c
adurmusoglu <durmusoglu555@gmail.com>
2025-01-27 16:55:11 -0600

    starting to implement js functioanlities

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// The logic of a web app for doing physics homework
+// Also learned how to build webpages with Javascript during this project
+
+console.log("app.js linked successfully!");
+
+// Buttons
+
+const xycomps = document.getElementById("xycomps");
+const magnitude = document.getElementById("magnitude");
+const addVectors = document.getElementById("add-vectors");
+const futureTopics = document.getElementById("future-topics");
+
+// Input areas
+
+const inputDefaultText = document.getElementById("default-text");
+const inputXYComps = document.getElementById("xycomps-input");
+const inputMagnitude = document.getElementById("magnitude-input");
+const inputAddVectors = document.getElementById("add-vectors-input");
+const inputFutureTopics = document.getElementById("future-topics-input");
+
+function hideAllTasks() {
+    inputDefaultText.classList.add("hidden");
+    inputXYComps.classList.add("hidden");
+    inputMagnitude.classList.add("hidden");
+    inputAddVectors.classList.add("hidden");
+    inputFutureTopics.classList.add("hidden");
+}
